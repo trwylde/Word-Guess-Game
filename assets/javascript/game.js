dcbb6eb8fa08bda751c4a51f329850c661eb8b9c
trwylde <trwylde@gmail.com>
2018-08-19 18:56:21 -0400

    Minor housekeeping

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,25 @@
 //document.getElementById("Hi").innerHTML = "Don't you wish you were here?!"; just a test... IT WORKS!!
     /*VARIABLES*/
     /*====================================================================================================================*/
-  var randomCityArr = ["ATHENS","BEIJING","BEIRUT","BERLIN","BOGOTA","GLASGOW","HAVANA","JOHANNESBURG","LISBON","LONDON","MADRID","MOSCOW","NAPLES","NASHVILLE","NEW YORK","PARIS","REYKJAVIK","RIO DE JANEIRO","SAN JUAN","TEL AVIV","TOKYO","VENICE"]; 
+  var cityArr = ["ATHENS","BEIJING","BEIRUT","BERLIN","BOGOTA","GLASGOW","HAVANA","JOHANNESBURG","LISBON","LONDON","MADRID","MOSCOW","NAPLES","NASHVILLE","NEW YORK","PARIS","REYKJAVIK","RIO DE JANEIRO","SAN JUAN","TEL AVIV","TOKYO","VENICE"]; 
     
+    //to control game start/stop
+var gameStatus = false;   
     
     /*The computer selects a city at random, the user must guess one letter at a time*/
-  var randomCity = randomCityArr[Math.floor(Math.random() * randomCityArr.length)];
+  var randomCity = cityArr[Math.floor(Math.random() * cityArr.length)];
       console.log (randomCity)
-      console.log (randomCityArr)
+      console.log (cityArr)
 
     /*all letters selected will be displayed as upper case value.toUpperCase();*/
     /* Wins & Losses set to 0. Guesses Remaining set to 10*/    
   
-  var userText = document.getElementById("player-guess");
+  var userText = document.getElementById("letters-crct");
   var wins = 0;
   var losses = 0;
   var guessesRemaining = 10;
 
-    
+   
     /*lettersIncArray empty at "function begin", will populate with letters incorrectly guessed. lettersCrctArray will be filled with 
     underscores, same number as in solution for randomly chosen word.*/
   var lettersIncArray = [];
@@ -28,11 +30,32 @@
     /*FUNCTIONS*/
     /*=====================================================================================================================*/
 
-    document.onkeyup = function(event) {
-      userText.textContent = event.key;
-    };
 
 
+    //var lettersArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+    function play() {
+      
+      for (var i = 0; i < randomCity.length; i++)
+      {
+      lettersCrctArray[i] = "_";
+      } /*closes for*/
+
+      /*joins them in a string*/
+      playerGuess = lettersCrctArray.join(" ");
+      document.getElementById("letters-crct").innerHTML = letters;
+    } /*closes function begin*/
+
+//    function Letter() {
+
+//      var letter = documentgetElementById("letter").value;
+//    }
+
+           
+
+          
+      
 
 
 
